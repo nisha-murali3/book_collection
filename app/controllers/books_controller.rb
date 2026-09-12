21392c57150d002811267f7924f649233034c941
Nisha Murali <nisha_murali@tamu.edu>
2026-09-12 14:12:37 -0500

    Book collection 2 done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   #look up book by URL
    before_action :set_book, only: [:show, :edit, :update, :delete, :confirm_delete]
 
+   #The index page should load all the books, so the view can list them
   def index
     @books = Book.all
   end
@@ -9,11 +10,14 @@ class BooksController < ApplicationController
   def show
   end
 
+  #Builds a new book 
   def new
     @book = Book.new
   end
 
-  #creating a new book, currently redirect to home page with a flash message
+  #Create a new book with the whitelisted parameters
+  #If it succeeds, redirect to the home page with a flash message
+  #Otherwise, re-render the new form, user can see validation errors and try again
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -26,7 +30,8 @@ class BooksController < ApplicationController
   def edit
   end
 
-#updating a book 
+#updating a book: attempt, redirect to home page with flash message upon success
+#Re-render if failed 
   def update
     if @book.update(book_params)
       redirect_to root_path, notice: "Book was successfully updated."
@@ -38,6 +43,7 @@ class BooksController < ApplicationController
   def confirm_delete
     end
 
+    #permanently remove book, redirect to home page 
   def delete
     @book.destroy
     redirect_to root_path, notice: "Book was successfully deleted."
@@ -49,7 +55,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  #Require title, author, price, and publish date to be present
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :author, :price, :published_date)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
+#a book model backed by the "books" data table
+#title, author, price, and published date must exist, price must be positive
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :published_date, presence: true
 end
