cf568e7e91553b10f2df5fd3a6c007fda6578869
Nisha Murali <nisha_murali@tamu.edu>
2026-09-14 20:43:44 -0500

    Add database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create!(title: "The Hobbit", author: "J.R.R. Tolkien", price: 12.99, published_date: Date.new(1937, 9, 21))
+Book.create!(title: "1984", author: "George Orwell", price: 10.99, published_date: Date.new(1949, 6, 8))
+Book.create!(title: "Dune", author: "Frank Herbert", price: 15.99, published_date: Date.new(1965, 8, 1))
+Book.create!(title: "Brave New World", author: "Aldous Huxley", price: 9.99, published_date: Date.new(1932, 1, 1))
+Book.create!(title: "Fahrenheit 451", author: "Ray Bradbury", price: 11.49, published_date: Date.new(1953, 10, 19))
